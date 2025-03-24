43aba967c5b2e63b35d7153a8f42446c6fb36d3a
Omar Ghassan <107811704+omarghassan@users.noreply.github.com>
2025-03-24 14:31:34 +0300

    Edit

--- database/factories/ProfileFactory.php
+++ database/factories/ProfileFactory.php
@@ -17,7 +17,13 @@ class ProfileFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            return [
+                'name' => fake()->name(),
+                'email' => fake()->unique()->safeEmail(),
+                'email_verified_at' => now(),
+                'password' => static::$password ??= Hash::make('password'),
+                'remember_token' => Str::random(10),
+            ];
         ];
     }
 }
--- database/seeders/ProfileSeeder.php
+++ database/seeders/ProfileSeeder.php
@@ -12,6 +12,6 @@ class ProfileSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Profile::factory(100)->create();
     }
 }
